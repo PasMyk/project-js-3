9c192a561dc068af153c7f67607a8839e9c8cffa
PasMyk <paskevicnikita6@gmail.com>
2025-09-30 18:43:43 +0100

    first commit

--- index.js
+++ index.js
@@ -50,13 +50,50 @@ console.log(computerNum);
 
 // Rock-Paper-Scissors
 
-const getStoreRps = document.querySelector("#game-stone-icon");
-const getScissorsRps = document.querySelector("#game-scissors-icon");
-const getPaperRps = document.querySelector("#game-paper-icon");
+const getItemsRps = Array.from(document.querySelectorAll(".rps-button"));
 const buttonComputerRps = document.querySelector("#game-rps-computer-show");
 const ComputerScoreRps = document.querySelector("#game-rps-computer-score");
 const UserScoreRps = document.querySelector("#game-rps-user-score");
+const resultTextRps = document.querySelector("#result-rps");
 
+let itemsRps = ["rock", "scissors", "paper"];
+let computerScore = 0;
+let userScore = 0;
+
+getItemsRps.forEach((element) => {
+  element.addEventListener("click", (event) => {
+    const userChoice = event.target.dataset.choice;
+    const compChoice = computerChoice(itemsRps); // зберігаємо вибір комп’ютера
+
+    buttonComputerRps.textContent = `Варіант комп’ютера: ${compChoice}`;
+
+    if (userChoice === compChoice) {
+      resultTextRps.textContent = "Нiчия!";
+      resultTextRps.classList.remove("game__result-incorrect");
+      resultTextRps.classList.add("game__result");
+    } else if (
+      (userChoice === "rock" && compChoice === "scissors") ||
+      (userChoice === "scissors" && compChoice === "paper") ||
+      (userChoice === "paper" && compChoice === "rock")
+    ) {
+      resultTextRps.textContent = "Ви виграли раунд!";
+      resultTextRps.classList.remove("game__result-incorrect");
+      resultTextRps.classList.add("game__result");
+      userScore++;
+      UserScoreRps.textContent = `Ви - ${userScore}`;
+    } else {
+      resultTextRps.textContent = "Комп’ютер виграв раунд!";
+      resultTextRps.classList.add("game__result-incorrect");
+      resultTextRps.classList.remove("game__result");
+      computerScore++;
+      ComputerScoreRps.textContent = `Комп’ютер - ${computerScore}`;
+    }
+  });
+});
+
+function computerChoice(choice) {
+  return choice[Math.floor(Math.random() * choice.length)];
+}
 // calculator
 
 const enterFirNumCalc = document.querySelector("#game-calc-enter-first-number");
@@ -70,61 +107,92 @@ const enterSecNumCalc = document.querySelector(
 const resultCalcButton = document.querySelector("#game-calc-result-button");
 const resultCalc = document.querySelector("#game-result-calc");
 
-let firstNumbers = [];
-let secondNumbers = [];
-let multiplesSymbols = [];
+const calcOp = Array.from(document.querySelectorAll(".calc__op"));
+let symb = "";
 
-let multiples = [];
-multiples.push(plusCalc, minusCalc, multiplyCalc, divideCalc);
-
-firstMovement = enterFirNumCalc.addEventListener("input", (event) => {
-  firstNumbers.push(enterFirNumCalc.value);
-  if (firstNumbers.length > 1 || enterFirNumCalc.value === "") {
-    firstNumbers.shift();
-  }
-  console.log(firstNumbers);
-});
-
-multiples.forEach((choiceMultiple) => {
-  choiceMultiple.addEventListener("click", (event) => {
-    multiplesSymbols.push(event.target);
-    if (multiplesSymbols.length > 1) {
-      alert("Button where deleted, press button again to get new value");
-      multiplesSymbols = [];
-    }
-    console.log(multiplesSymbols);
+calcOp.forEach((elem) => {
+  elem.addEventListener("click", (e) => {
+    symb = e.target.textContent;
   });
 });
 
-ThirdMovement = enterSecNumCalc.addEventListener("input", (event) => {
-  secondNumbers.push(enterSecNumCalc.value);
-  if (secondNumbers.length > 1 || enterSecNumCalc.value === "") {
-    secondNumbers.shift();
+resultCalcButton.addEventListener("click", () => {
+  const firstNumbers = enterFirNumCalc.value;
+  const secondNumbers = enterSecNumCalc.value;
+
+  switch (symb) {
+    case plusCalc.textContent:
+      resultCalc.textContent = Number(firstNumbers) + Number(secondNumbers);
+      break;
+    case minusCalc.textContent:
+      resultCalc.textContent = Number(firstNumbers) - Number(secondNumbers);
+      break;
+    case multiplyCalc.textContent:
+      resultCalc.textContent = Number(firstNumbers) * Number(secondNumbers);
+      break;
+    case divideCalc.textContent:
+      resultCalc.textContent = Number(firstNumbers) / Number(secondNumbers);
+      break;
+    default:
+      resultCalc.textContent = "Оберіть дію";
+      break;
   }
-  console.log(secondNumbers);
 });
 
-resultCalcButton.addEventListener("click", (event) => {
-  switch (multiples) {
-    case plusCalc:
-      resultCalc.textContent =
-        Number(firstNumbers[0]) + Number(secondNumbers[0]);
-      break;
-    case minusCalc:
-      resultCalc.textContent =
-        Number(firstNumbers[0]) - Number(secondNumbers[0]);
-      break;
-    case multiplyCalc:
-      resultCalc.textContent =
-        Number(firstNumbers[0]) * Number(secondNumbers[0]);
-      break;
-    case divideCalc:
-      resultCalc.textContent =
-        Number(firstNumbers[0]) / Number(secondNumbers[0]);
-      break;
+// time calculator
 
-    default:
+const getInputCalcTime = document.querySelector(".time-calculator__input");
+const getButtonCalcTime = document.querySelector(".game__timeCalc-button");
+const getResultCalcTime = document.querySelector(".game__result-CalcTime");
+
+function calcTime(minutes) {
+  const days = Math.floor(minutes / 1440);
+  const hours = Math.floor((minutes % 1400) / 60);
+  const remainingMinutes = minutes & 60;
+
+  return (getResultCalcTime.textContent = `${days}дн. ${hours}:${remainingMinutes}`);
+}
+
+getButtonCalcTime.addEventListener("click", (e) => {
+  calcTime(getInputCalcTime.value.trim());
+});
+
+console.log(calcTime(getInputCalcTime.value));
+
+// football
+
+const getFootballBall = document.querySelector(".football-object");
+const getFootballField = document.querySelector(".game__field");
+
+getFootballField.addEventListener("mousemove", (e) => {
+  let pos = e.offsetX;
+});
+
+// biggest number
+
+const getMaxNumResult = document.querySelector(".game__maxNumResult");
+const labelInp = document.querySelector(".game__input-group");
+
+let arr = {
+  num1: 0,
+  num2: 0,
+  num3: 0,
+};
+
+labelInp.addEventListener("input", (e) => {
+  switch (e.target.dataset.num) {
+    case "num1":
+      arr = { ...arr, num1: e.target.value };
+      break;
+    case "num2":
+      arr = { ...arr, num2: e.target.value };
+      break;
+    case "num3":
+      arr = { ...arr, num3: e.target.value };
       break;
   }
-  console.log(resultCalc.textContent);
+  let { num1, num2, num3 } = arr;
+  console.log(arr);
+
+  getMaxNumResult.textContent = Math.max(num1, num2, num3);
 });
